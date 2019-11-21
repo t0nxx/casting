4cc7a96371b5cec215d2639501535fe2328c47c0
t0nxx <mahmoudko1500@hotmail.com>
2019-11-21 16:40:52 +0200

    fix get one post req method

--- src/routes/ActivityRoute.ts
+++ src/routes/ActivityRoute.ts
@@ -10,7 +10,7 @@ const commentController = new CommentController();
 router.get('/', AuthMiddleWare, activityController.getAllActivityTest);
 router.post('/', AuthMiddleWare, activityController.AddNewActivity);
 router.get('/saved', AuthMiddleWare, activityController.getAllBookmarkedActivity);
-router.post('/:id', AuthMiddleWare, activityController.getOneActivity);
+router.get('/:id', AuthMiddleWare, activityController.getOneActivity);
 router.post('/:id/like', AuthMiddleWare, activityController.LikeActivity);
 router.post('/:id/unlike', AuthMiddleWare, activityController.DisLikeActivity);
 router.post('/:id/bookmark', AuthMiddleWare, activityController.BookmarkActivity);
