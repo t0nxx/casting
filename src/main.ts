d5957258bc31f1ededfb5316cdbb6d58bcd08107
t0nxx <mahmoudko1500@hotmail.com>
2020-04-02 19:15:07 +0200

    up to memory

--- src/main.ts
+++ src/main.ts
@@ -75,8 +75,6 @@ createConnection().then(async connection => {
     }));
     app.use(fileupload({
         limits: { fileSize: 100 * 1024 * 1024 },
-        useTempFiles: true,
-        tempFileDir: '/tmp/',
     }));
 
     // join chat rooms middleware
