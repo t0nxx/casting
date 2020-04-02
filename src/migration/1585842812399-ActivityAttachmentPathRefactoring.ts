acd6a40065613711d111934b421da2963debe346
t0nxx <mahmoudko1500@hotmail.com>
2020-04-02 18:05:49 +0200

    add activity attachment migration 2

--- src/migration/1585842812399-ActivityAttachmentPathRefactoring.ts
+++ src/migration/1585842812399-ActivityAttachmentPathRefactoring.ts
@@ -3,11 +3,12 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class ActivityAttachmentPathRefactoring1585842812399 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query('ALTER TABLE `activity_attachment` CHANGE `path` `path` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;');
+        await queryRunner.query('ALTER TABLE `activity_attachment` MODIFY path TEXT NOT NULL;');
     }
 
+
     public async down(queryRunner: QueryRunner): Promise<any> {
-        await queryRunner.query('ALTER TABLE `activity_attachment` CHANGE `path` `path` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL;');
+        await queryRunner.query('ALTER TABLE `activity_attachment` MODIFY path TEXT NOT NULL;');
     }
 
 }
